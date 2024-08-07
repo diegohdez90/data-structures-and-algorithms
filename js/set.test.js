84a886e0c5c44f819b7bc5fe0ed23f5aa8d3a427
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2024-08-07 13:27:53 -0600

    test: add test for set

---
+++ js/set.test.js
@@ -0,0 +1,29 @@
+import LinkedList from "./classes/index.js";
+
+describe('set value', () => {
+    it('set existed index', () => {
+        const ll = new LinkedList()
+        ll.push(13)
+        ll.push(47)
+        ll.push(31)
+        ll.push(16)
+        ll.push(7)
+        ll.push(23)
+        ll.push(18)
+        ll.push(37)
+        ll.set(2, 22)
+        expect(ll.getById(2).value).toBe(22)
+    })
+    it('set false for index not found', () => {
+        const ll = new LinkedList()
+        ll.push(13)
+        ll.push(47)
+        ll.push(31)
+        ll.push(16)
+        ll.push(7)
+        ll.push(23)
+        ll.push(18)
+        ll.push(37)
+        expect(ll.set(10, 33)).toBeFalsy()
+    })
+})
