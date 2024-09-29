1504ebf3c3481ff0df69b871d0783ffadae1809b
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2024-09-29 16:49:23 -0600

    feat: add push method

--- js/classes/DoubleLinkedList/class.js
+++ js/classes/DoubleLinkedList/class.js
@@ -11,6 +11,20 @@ class DoubleLinkedList {
     this.head = node
     this.tail = node
   }
+
+  push(v) {
+    const node = new NodeDouble(v)
+    if (this.length === 0) {
+      this.head = node
+      this.tail = node
+    } else {
+      this.tail.next = node
+      node.prev = this.tail
+      this.tail = node
+    }
+    this.length++
+    return this
+  }
 }
 
 export default DoubleLinkedList
