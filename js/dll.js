a00c17ae80d143cf15321b9c00cc77826f303e5c
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2024-09-29 15:54:42 -0600

    feat: add sample dll

---
+++ js/dll.js
@@ -0,0 +1,11 @@
+import { DoubleLinkedList } from './classes/index.js'
+
+const dll = new DoubleLinkedList(4)
+
+console.log(dll.length);
+console.log(dll.head);
+console.log(dll.tail);
+console.log(dll.head.next);
+console.log(dll.head.prev);
+
+
