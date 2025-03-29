07c3a9c55f42184332585cb4ad45fd17f862ef62
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-29 17:26:30 -0600

    feat(queue): add dequeue method

--- js/classes/Queue/class.js
+++ js/classes/Queue/class.js
@@ -19,6 +19,20 @@ class Queue {
     }
     this.length++
   }
+
+  dequeue() {
+    if (this.length == 0) return undefined
+    var tmp = this.first
+    if (this.length == 1) {
+      this.first = null
+      this.last = null
+    } else {
+      this.first = tmp.pointer
+      tmp.pointer = null
+    }
+    this.length--
+    return tmp
+  }
 }
 
 export default Queue
