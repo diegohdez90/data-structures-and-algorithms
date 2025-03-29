2e6d76bcf28a4549f844cf1650c4c04b092b88fa
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-29 17:07:31 -0600

    feat(queue): add enqueue method to add items

--- js/classes/Queue/class.js
+++ js/classes/Queue/class.js
@@ -7,6 +7,18 @@ class Queue {
     this.last = node
     this.length = 1
   }
+
+  enqueue(value) {
+    var node = new Node(value)
+    if (this.length == 0) {
+      this.first = node
+      this.last = node
+    } else {
+      this.last.pointer = node
+      this.last = node
+    }
+    this.length++
+  }
 }
 
 export default Queue
