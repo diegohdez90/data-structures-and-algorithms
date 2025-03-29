1686c74167c812232aa497571b352048c1040d2b
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-29 15:54:44 -0600

    feat(stack): add push method

--- js/classes/Stack/class.js
+++ js/classes/Stack/class.js
@@ -6,6 +6,18 @@ class Stack {
     this.top = node
     this.length = 1
   }
+
+  push(value) {
+    var node = new Node(value)
+    if (this.length == 0) {
+      this.top = node
+    } else {
+      node.pointer = this.top
+      this.top = node
+    }
+    this.length++
+    return this
+  }
 }
 
 export default Stack
