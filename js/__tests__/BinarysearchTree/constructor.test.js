87128bb8e55e8bff2ad2f8e76a944ac0072da680
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-29 20:51:32 -0600

    test(bst): empty binary search tree

---
+++ js/__tests__/BinarysearchTree/constructor.test.js
@@ -0,0 +1,8 @@
+import { BinarySearchTree } from "../../classes";
+
+describe('binary search tree', () => {
+  it('constructor', () => {
+    var bst = new BinarySearchTree()
+    expect(bst.root).toBeNull()
+  })
+})
