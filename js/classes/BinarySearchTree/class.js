7be76514ec9a6feba3eccf957bfd8bcd346ebd64
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-03-31 18:52:43 -0600

    feat(bst): add insert method to add values

--- js/classes/BinarySearchTree/class.js
+++ js/classes/BinarySearchTree/class.js
@@ -4,6 +4,32 @@ class BinarySearchTree {
   constructor(value) {
     this.root = null
   }
+
+  insert(value) {
+    var node = new TreeNode(value)
+    if (this.root == null) {
+      this.root = node
+      return this
+    }
+    let tmp = this.root
+    while(true) {
+      if (node.value === tmp.value)
+        return false
+      if (node.value < tmp.value) {
+        if (tmp.left === null) {
+          tmp.left = node
+          return true
+        }
+        tmp = tmp.left
+      } else {
+        if (tmp.right === null) {
+          tmp.right = node
+          return true
+        }
+        tmp = tmp.right
+      }
+    }
+  }
 }
 
 export default BinarySearchTree
