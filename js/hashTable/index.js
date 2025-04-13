ff119dbb0fbba40ef0800331535daa9594f9d3f3
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2025-04-12 18:08:22 -0600

    docs(hashTable): add how to use module for hash map sample

--- js/hashTable/index.js
+++ js/hashTable/index.js
@@ -3,5 +3,7 @@ import { HashTable } from "../classes/index.js";
 var hashTable = new HashTable()
 
 /**
- * TODO: implement via debug how to set and get in hash map
+ * TODO: implement via debug how to set and get in hash 
+ * masrk brekpoint in the line 9
  */
+hashTable
